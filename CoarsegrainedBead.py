fixed quaternion being printed with real part as last; updated printing xml statements; proper multiplication of quaternions; more typos

CoarsegrainedBead.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np
 
 
@@ -21,7 +23,13 @@ class bead(object):
         if quaternion is None:
             self.orientation = np.array([0.0, 0.0, 0.0, 0.0])
         else:
-            self.orientation = quaternion.q
+            try:
+                self.orientation = quaternion.q_w_ijk
+            except AttributeError:  # In case whatever is passed doesnt support q_w_ijk;
+                warnings.warn(
+                    'the quaternion passed doesnt support q_w_ijk; errors may be encountered while printing XML',
+                    UserWarning)
+                self.orientation = quaternion
         if moment_inertia is None:
             self.moment_inertia = np.array([0.0, 0.0, 0.0])
         else:

Colloid.py
@@ -106,6 +106,7 @@ class Colloid(object):
         self.body_num = 0
         self.body_beads = []
         self.body_mass = 0.0
+        self.__initial_rotation()
 
     @property
     def surface_type(self):
@@ -229,6 +230,13 @@ class Colloid(object):
             _.append((position[0], position[1], position[2]))
         return _
 
+    def __initial_rotation(self):
+        for pidx in range(self._sh.table.__len__()):
+            lvec = vec(copy.deepcopy(self._sh.table[pidx]))
+            lvec.rot(mat=self.quaternion.transform)
+            self._sh.table[pidx] = lvec.array
+
+
     def rotate(self, operation):
         """
         rotates the colloid
@@ -239,14 +247,14 @@ class Colloid(object):
         self.pos = self.pos - _t
         q_op = Quat(operation)
         r_mat = q_op.transform
-        for i in range(self.body_beads.__len__(), self.pos.__len__()):
+        for i in range(0, self.pos.__len__()):
             _tmp_dump = vec(self.pos[i, :])
             _tmp_dump.rot(mat=r_mat)
             self.pos[i,:] = _tmp_dump.array
             self.beads[i].position = _tmp_dump.array + _t
             del _tmp_dump
         self.pos = self.pos + _t
-        self.quaternion *= q_op
+        self.quaternion = q_op * self.quaternion
 
     def graft_EXT(self, EXT_IDX, rem_id, num, linker_type):
         """
@@ -271,15 +279,22 @@ class Colloid(object):
 
         for obj_copy_idx in range(num):
             _dump_copy = copy.deepcopy(self._sh.ext_objects[EXT_IDX])
+
+            # try setting some random rotation to avoid overlaps
+            try:
+                _dump_copy.randomize_dirs()
+            except AttributeError:
+                pass
+
             _p_off = self.p_num[-1]+1
             _att_vec = vec(copy.deepcopy(self.pos[self.att_list[-1][obj_copy_idx], :] - self.pos[0, :]))
-            _att_vec.rot(mat=self.quaternion.transform)
             _rot_matrix = get_rot_mat(_att_vec.array)
             for obj_int_crd in range(_dump_copy.beads.__len__()):
                 _v = vec(_dump_copy.beads[obj_int_crd].position)
                 _v.rot(_rot_matrix)
                 _dump_copy.beads[obj_int_crd].position = self.pos[0, :] + _att_vec.array * (
-                1.00 + 0.8 / _att_vec.__norm__()) + _v.array
+                    1.00 + 0.8 / _att_vec.__norm__()) + _v.array + np.array(
+                    [random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)]) * 0.1
                 del _v
                 self.pos = np.append(self.pos, np.array([_dump_copy.beads[obj_int_crd].position[:]]), axis = 0)
                 self.beads.append(_dump_copy.beads[obj_int_crd])
@@ -317,7 +332,6 @@ class SimpleColloid(Colloid):
         self.beads = [CoarsegrainedBead.bead(position=np.array([0.0, 0.0, 0.0]), beadtype=self.c_type, body=0,
                                              mass=self.mass, quaternion=self.quaternion,
                                              moment_inertia=self.diagI * self.mass * (self.size ** 2.0))]
-
         self.__build_surface()
         # check if the system is rigid
         if not self.bonds.__len__() == 0:
@@ -335,7 +349,7 @@ class SimpleColloid(Colloid):
         self.beads += [CoarsegrainedBead.bead(position=copy.deepcopy(self._sh.pos[i] * self.size / 2.0),
                                               beadtype=self.s_type, body=0, mass=self.s_mass) for i in
                        range(self._sh.pos.__len__())]
-        self.body_beads += [self.beads[i] for i in range(1, self.beads.__len__())]
+        self.body_beads += [self.beads[i] for i in range(0, self.beads.__len__())]
 
     def __build_grafts(self):
         if 'EXT' in self._sh.keys:
@@ -366,7 +380,7 @@ class ComplexColloid(Colloid):
             self.nshells = self._sh.flags['multiple_surface_types'].__len__()
         else:
             self.nshells = 1
-
+        self.body_beads += self.beads
         self.__build_shells()
         # check if the system is rigid
         if not self.bonds.__len__() == 0:

GenShape.py
@@ -162,7 +162,7 @@ class shape(object):
                 dmp_vec = vec(self.table[i, :])
                 dmp_vec.rot(mat=self.srot_mat)
                 self.table[i, :] = dmp_vec.array
-            self.quaternion *= Quat(self.srot_mat)
+            self.quaternion = Quat(self.srot_mat) * self.quaternion
         except AttributeError:
             pass
 
@@ -170,7 +170,7 @@ class shape(object):
         if operator is None:
             operator = np.eye(3)
         Q_op = Quat(operator)
-        self.quaternion *= Q_op
+        self.quaternion = Q_op * self.quaternion
 
     def Set_Geometric_Quaternion(self):
         # calculate the initial tensor
@@ -192,7 +192,6 @@ class shape(object):
         idx = w.argsort(kind='mergesort')[::-1]
         w = w[idx]
         v = v[:, idx]
-
         # invert one axis if the coordinate system has become left-handed
         if np.linalg.det(v) < 0:
             v[:, 0] = -v[:, 0]
@@ -200,6 +199,19 @@ class shape(object):
         self.quaternion = Quat(v)
         self.Itensor = np.array([w[0], w[1], w[2]], dtype=float)
 
+    def get_N_largest(self, N):
+        """
+        gets the N largest indexes of norm values in self.table and adds one to the result to offset the center.
+        This is made for geometric shapes.
+        :param N number of largest norm values to return
+        :return list of indexes
+        """
+        _norm = np.zeros(self.table.__len__(), dtype=float)
+        for idx in range(self.table.__len__()):
+            _norm[idx] = self.table[idx, 0] ** 2.0 + self.table[idx, 1] ** 2.0 + self.table[idx, 2] ** 2.0
+        args = np.argsort(_norm)
+        args = args[0: N] + 1
+        return args.tolist()
 
 class Cube(shape):
     def __init__(self, Num,  surf_plane = None, lattice = None, properties = None, Radius = None):
@@ -399,7 +411,7 @@ class PdbProtein(shape):
         :return:
         """
 
-        with open(filename, 'r') as f:
+        with open(filename) as f:
             self._pdb = f.readlines()
         del f
 
@@ -691,7 +703,7 @@ class Tetrahedron(shape):
         FacePoint = Table.__len__()
         Table /= np.max(Table)
         _ang = acos(1.0 / 3.0)
-        _r_mat = self.mat_from_plane(plane= [0.0, sin(_ang), cos(_ang)])
+        _r_mat = get_rot_mat([0.0, sin(_ang), cos(_ang)])
 
 
         TableZ = np.copy(Table)

Util.py
@@ -85,14 +85,14 @@ def read_centers(f_center):
     lines = fc.readlines()[2:]
 
     positions = []
-    type = []
+    types = []
     for line in lines:
         l = line.strip().split()
         pos = [float(l[1])*1.0, float(l[2])*1.0, float(l[3])*1.0]
         positions.append(pos)
-        type.append(l[0])
+        types.append(l[0])
 
-    return type, positions
+    return types, positions
 
 def get_rot_mat(cubic_plane):
     _cp = vector(cubic_plane)
@@ -107,7 +107,7 @@ def get_rot_mat(cubic_plane):
 
     if _v.array[0] == 0.0 and _v.array[1] == 0.0: # surface is [001]
         return _id
-    return  np.linalg.inv(_id + _mat_vx + (1.0-_cl) / _sl**2 * np.linalg.matrix_power(_mat_vx, 2))
+    return _id + _mat_vx + (1.0 - _cl) / _sl ** 2 * np.linalg.matrix_power(_mat_vx, 2)
 
 def get_inv_rot_mat(cubic_plane):
     _cp = vector(cubic_plane)
@@ -122,7 +122,7 @@ def get_inv_rot_mat(cubic_plane):
 
     if _v.array[0] == 0.0 and _v.array[1] == 0.0: # surface is [001]
         return _id
-    return  _id + _mat_vx + (1.0-_cl) / _sl**2 * np.linalg.matrix_power(_mat_vx, 2)
+    return np.linalg.inv(_id + _mat_vx + (1.0 - _cl) / _sl ** 2 * np.linalg.matrix_power(_mat_vx, 2))
 
 
 def gen_random_mat():

Walls.py
@@ -1,51 +1,13 @@
 __author__ = 'martin'
-import numpy as np
-import CoarsegrainedBead as CG
 import copy
 import random
+from math import *
 
+import numpy as np
 
-class vec(object):
-        """
-        simple vector class with useful tools; should be reusable
-        """
-        def __init__(self, array, ntol = 1e-5):
-            self.array = np.array(array, dtype=float)
-            self.ntol = ntol
-            if array.__len__() <1:
-                del self
-        def __neg__(self):
-            self.array = -self.array
-            return self
-        @property
-        def x(self):
-            return self.array[0]
-        @property
-        def y(self):
-            return self.array[1]
-        @property
-        def z(self):
-            return self.array[2]
-
-        def __norm__(self):
-            _c = 0.0
-            for i in range(self.array.__len__()):
-                _c += self.array[i]**2
-            _c **= 0.5
-            return _c
-        def normalize(self):
-            self.array /= self.__norm__()
-        def rot(self,mat):
-            self.array = mat.dot(self.array)
-        def inv_rot(self, mat):
-            if self.x != 0 or self.y != 0 or self.z !=0:
-                self.array = np.linalg.solve(mat, np.array([0.0, 0.0, 1.0])*self.__norm__())
-        def x_prod_by001(self):
-            _c = vec([-self.array[1], self.array[0], 0.0])
-            return _c
-        def i_prod_by001(self):
-            _c = self.array[2]
-            return _c
+import CoarsegrainedBead as CG
+from Util import get_rot_mat
+from Util import vector as vec
 
 
 class Wall(object):
@@ -136,7 +98,7 @@ class Wall(object):
         _r_th = random.uniform(0, 2*pi)
         _r_z = random.uniform(0,1)
         _r_ori = [(1-_r_z**2)**0.5*cos(_r_th), (1-_r_z**2)**0.5*sin(_r_th), _r_z]
-        return LinearChain.get_rot_mat(_r_ori)
+        return get_rot_mat(_r_ori)
 
     def random_rotation(self):
         _r_mat = self.random_rot_matrix()